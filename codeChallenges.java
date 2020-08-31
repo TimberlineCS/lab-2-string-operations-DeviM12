555ad1ff284c6b677eadba3bc9be83c2d5663d9d
Devi Alekhya Medicharla <devi.medicharla2022@gmail.com>
2020-08-31 19:10:34 +0000

    Done

---
+++ codeChallenges.java
@@ -0,0 +1,28 @@
+public class codeChallenges{
+
+  /*Devi Alekhya Medicharla
+  Computer Science Period 3
+  8/31/2020*/
+  public static void main(String args[]){
+   //Challenge 1
+
+    String word = "Scissors";
+    int wordLength = word.length();
+    int midpoint = wordLength / 2;
+    String firstHalf = word.substring(0,midpoint);
+    String secHalf = word.substring(midpoint);
+    String reverse = secHalf + firstHalf;
+    String lowercaseword = reverse.toLowerCase();
+    System.out.println(lowercaseword);
+
+    //Challenge 2
+    
+   
+    
+    System.out.println
+    ("        __\n (_____()'`;\n /,     /,-\n \\\\\"----\\\\\"");
+
+  }
+
+  
+}
